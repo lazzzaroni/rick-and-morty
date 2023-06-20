0f7d33104f31796b53ffb68dec6949b6eced5188
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-06-20 12:06:42 +0200

    add axios & react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+  type DehydratedState,
+} from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
